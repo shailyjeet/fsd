e7c3deffe21d5bf913eb46f70f70fd7cba83e9a6
Jeet J <shailyjeet@gmail.com>
2021-07-26 21:53:22 -0400

    Sprint 1: Added Project Algorithm and Sprint Planning Documentation

---
+++ com.fsd1.Typecast/src/com/fsd1/LockerPvtLtd/runApp.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.fsd1.LockerPvtLtd;
+
+/**
+ * @author JJ
+ *
+ */
+public class runApp {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+		System.out.println(":::::::::::::::::::::LockedMe.com :::::::::::::::::::::::::::::");
+		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+		System.out.println(":::::::::::::::Application created by: JJ::::::::::::::::::::::");
+		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+		System.out.println("please select from the below options: ");
+		System.out.println("  ");
+		System.out.println("Enter 1. to get the file name in the directory ");
+		System.out.println("Enter 2. to search, add or delete files in the directory");
+		System.out.println("Enter 3. to Exit the Application");
+
+	}
+
+}
+
+
+
+
